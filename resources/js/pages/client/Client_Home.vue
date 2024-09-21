f5ae6b0878245252d5b5b2644d737c025437468d
Kiel Alarcon Bermudez <kielbermudez200104@gmail.com>
2024-09-21 23:33:54 +0800

    test1

--- resources/js/pages/client/Client_Home.vue
+++ resources/js/pages/client/Client_Home.vue
@@ -168,7 +168,7 @@ import Navbar from '@/components/Client_Navbar.vue'
                 </figure>
                 <summary>
                     <p>10 Simple steps to revolutionise work flows with our project</p>
-                    <small>₱400.00 PHP</small>
+                    <small>₱20.00 PHP</small>
                 </summary>
             </article>
             <article>
