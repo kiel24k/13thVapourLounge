5885f884c07a64875d41b8d457c9d9f7883b26ac
Kiel Alarcon Bermudez <kielbermudez200104@gmail.com>
2024-09-25 23:34:05 +0800

    cart fixed for gap UI

--- resources/js/components/Client_Navbar.vue
+++ resources/js/components/Client_Navbar.vue
@@ -8,7 +8,7 @@
                 <div class="">
                     <img src="/public/image/1920525_gogle_google_logo_network_icon.png" alt="" width="40">
                 </div>
-                <div class="">
+                <div class="header-action">
                     <img src="/public/image/370076_account_avatar_client_male_person_icon.png" alt="" width="30">
                     <span>Cart / P280.00</span>
                     <img src="/public/image/cart1-icon.png" alt="" width="30">
@@ -25,5 +25,9 @@
     box-shadow: 0px 0px 5px 0px gray;
     padding: 10px;
     align-items: center;
+}
+.header-action{
+    display: flex;
+    gap:10px;
 }
 </style>
