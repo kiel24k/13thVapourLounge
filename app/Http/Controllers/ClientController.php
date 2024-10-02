01a1b39d8a28cd9c420a7c724fd5266dbfb57dcd
Kiel Alarcon Bermudez <kielbermudez200104@gmail.com>
2024-10-02 22:43:44 +0800

    fetch product on client side => done

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
-    
+    public function newArrivalList()
+    {
+        $product = Product::where('label_category', 'new-arrival')->paginate(5);
+        return response()->json($product);
+    }
+    public function bestSeller() {
+        $product = Product::where('label_category', 'best-seller')->paginate(5);
+        return response()->json($product);
+    }
 }
