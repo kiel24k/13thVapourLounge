b395e6ee268c0466da597d259d42ab34c221b4ed
Kiel Alarcon Bermudez <kielbermudez200104@gmail.com>
2024-10-20 23:58:53 +0800

    test

--- resources/js/pages/client/Client_Profile.vue
+++ resources/js/pages/client/Client_Profile.vue
@@ -3,12 +3,27 @@ import Header from '@/components/Client_Header.vue'
 import Navbar from '@/components/Client_Navbar.vue'
 import NavbarCategory from '@/components/Client_Navbar_Category.vue'
 import NavbarAccount from '@/components/Client_Navbar_Account.vue'
+import { onMounted, ref } from 'vue';
+const user = ref(Object)
+const USER_API = async () => {
+    try {
+        const response = await axios.get('/api/user')
+        user.value = response.data
+    } catch (error) {
+
+    }
+}
+
+onMounted(() => {
+    USER_API()
+
+})
 </script>
 
 <template>
-    <Header/>
-    <Navbar/>
-    <NavbarCategory/>
+    <Header />
+    <Navbar />
+    <NavbarCategory />
     <section class="row" id="profile">
         <div class="col">
             <figure>
@@ -28,85 +43,99 @@ import NavbarAccount from '@/components/Client_Navbar_Account.vue'
     </section>
 
     <nav>
-        <NavbarAccount/>
+        <NavbarAccount />
     </nav>
 
     <section id="info">
-        <article>
-            <h2>Info</h2>
-            <div class="content">
-                <figure class="figure-info">
-                      <img src="/public/image/5296499_fb_facebook_facebook logo_icon.png" width="50" alt="">
-                      <figcaption class="figure-figcaption">
-                        emailsample@gmail.com
-                      </figcaption>
-                </figure>
+        <article class="profile">
+            <div class="row">
+                <div class="col">
+                    <label for="">Full Name</label>
+                    <span>{{user.first_name}} {{ user.last_name }}</span>
+                </div>
+                <div class="col">
+                    <label for="">Email Address</label>
+                    <span>{{ user.email }}</span>
+                </div>
+                <div class="col">
+                    <label for="">Mobile</label>
+                    <span>{{ user.phone }}</span>
+                </div>
             </div>
-            <div class="content">
-                <figure class="figure-info">
-                      <img src="/public/image/5296499_fb_facebook_facebook logo_icon.png" width="50" alt="">
-                      <figcaption class="figure-figcaption">
-                        emailsample@gmail.com
-                      </figcaption>
-                </figure>
+            <div class="row">
+                <div class="col col-4">
+                    <label for="">Birthday</label>
+                    <span>2001-12-04</span>
+                </div>
+                <div class="col">
+                    <label for="">Gender</label>
+                    <span>Male</span>
+                </div>
             </div>
-            <div class="content">
-                <figure class="figure-info">
-                      <img src="/public/image/5296499_fb_facebook_facebook logo_icon.png" width="50" alt="">
-                      <figcaption class="figure-figcaption">
-                        emailsample@gmail.com
-                      </figcaption>
-                </figure>
-            </div>
-            <div class="content">
-                <figure class="figure-info">
-                      <img src="/public/image/5296499_fb_facebook_facebook logo_icon.png" width="50" alt="">
-                      <figcaption class="figure-figcaption">
-                        emailsample@gmail.com
-                      </figcaption>
-                </figure>
-            </div>
+
+
         </article>
+        <div class="action">
+            <button class="btn btn-primary">Edit</button>
+            <button class="btn btn-danger">Change Password</button>
+        </div>
     </section>
 </template>
 
 <style scoped>
-    #profile {
-        max-width: 70%;
-        margin: auto;
-        display: flex;
-        flex-wrap: wrap;
-    }
+#profile {
+    max-width: 70%;
+    margin: auto;
+    display: flex;
+    flex-wrap: wrap;
+}
 
-    figure {
-        display: flex;
-        align-items: center;
-        gap: 10px
-    }
+figure {
+    display: flex;
+    align-items: center;
+    gap: 10px
+}
 
-    .profile-figcaption {
-        font-weight: 600;
-    }
+.profile-figcaption {
+    font-weight: 600;
+}
 
-    .profile-action {
-        display: flex;
-        justify-content: end;
-        align-items: center;
-    }
+.profile-action {
+    display: flex;
+    justify-content: end;
+    align-items: center;
+}
 
-    #info{
-        max-width: 70%;
-        margin:auto;
-        box-shadow: 0px 0px 2px 0px gray;    
-        border-radius: 10px;
-        padding:10px;
-        
-    }
+#info {
+    max-width: 70%;
+    margin: auto;
+    padding: 10px;
+    background: white;
+}
 
-    .figure-info {
-        display: flex;
-    }
-    .figure-figcaption{
-        font-weight: 600;
-    }
+#info .profile .col {
+    display: grid;
+    gap: 10px;
+    margin-top: 20px;
+}
+
+#info .profile .col label {
+    font-size: 12px;
+}
+
+#info .profile .col span {
+    font-size: 14px;
+    color: rgb(2, 92, 92);
+}
+
+.profile {
+    display: grid;
+}
+
+.action {
+    display: grid;
+    justify-content: start;
+    gap: 10px;
+    margin-top: 10px;
+}
 </style>
