1313b77722047304de7ffae7833a085c4e0c4350
Kiel Alarcon Bermudez <kielbermudez200104@gmail.com>
2024-10-31 23:15:33 +0800

    test3

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -32,7 +32,7 @@ class DashboardController extends Controller
         ->selectRaw('COUNT(*) as item_count')
         ->groupBy('order_label')
         ->get();
-//sdad
+//sdadsd
         return response()->json($products);
     }
 }
