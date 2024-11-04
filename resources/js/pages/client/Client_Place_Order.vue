09a281b97dd94d11a9dbad6441eb7ea865eff238
Kiel Alarcon Bermudez <kielbermudez200104@gmail.com>
2024-11-04 11:45:49 +0800

    place order UI added

--- resources/js/pages/client/Client_Place_Order.vue
+++ resources/js/pages/client/Client_Place_Order.vue
@@ -3,6 +3,57 @@ import Header from '@/components/Client_Header.vue'
 import Navbar from '@/components/Client_Navbar.vue'
 import NavbarCategory from '@/components/Client_Navbar_Category.vue'
 import Footer from '@/components/Client_Footer.vue'
+import { useRoute, useRouter } from 'vue-router'
+import { onMounted, ref } from 'vue'
+import Cookies from 'js-cookie'
+import ChooseAddress from '@/components/Client_Choose_Address_Modal.vue'
+import Swal from 'sweetalert2'
+
+
+const route = useRoute()
+const overAllTotalParams = parseInt(route.query.total) + 500
+const order = ref()
+const router = useRouter()
+
+
+
+const PRODUCTS = () => {
+    const paramsId = route.query.item_id.map(Number)
+    const cartItems = JSON.parse(localStorage.getItem('cart') || [])
+    const getIdOnCart = cartItems.filter((el) => paramsId.includes(el.id))
+    order.value = getIdOnCart
+}
+
+const chooseAddress = () => {
+   
+
+}
+
+const submit = async () => {
+    let order = JSON.parse(localStorage.getItem('cart'))
+    let cookieUsername = JSON.stringify(Cookies.get('username'))
+    if (cookieUsername) {
+        if (FIXED_TOTAL.value === 0) {
+            alert("No Product ")
+        } else {
+            const orderData = order.filter((el) => el.quantity > 0)
+            const response = await axios.post('/api/client-order', {
+                user_id: userData.value.id,
+                order_data: orderData
+            })
+            localStorage.setItem('cart', JSON.stringify(order.filter((el) => el.quantity === 0)))
+        }
+
+    } else {
+
+        router.push('/login')
+    }
+    fetchProducts()
+    SUB_TOTAL()
+}
+onMounted(() => {
+    PRODUCTS()
+})
 </script>
 
 <template>
@@ -10,10 +61,10 @@ import Footer from '@/components/Client_Footer.vue'
     <Navbar />
     <NavbarCategory />
     <section class="section-one">
-        <div class="address">
+        <div class="address" ref="test">
             <div class="row">
                 <div class="col">
-                    Already Address? <a href="">Choose Address</a>
+                    Already Address? <small class="text-primary" @click="chooseAddress">Choose Address</small>
                 </div>
             </div>
             <div class="row">
@@ -23,10 +74,10 @@ import Footer from '@/components/Client_Footer.vue'
             </div>
             <div class="row">
                 <div class="col">
-                    <input type="text" placeholder="First Name" >
+                    <input type="text" placeholder="First Name">
                 </div>
                 <div class="col">
-                    <input type="text" placeholder="Last Name" >
+                    <input type="text" placeholder="Last Name">
                 </div>
             </div>
             <div class="row">
@@ -35,13 +86,13 @@ import Footer from '@/components/Client_Footer.vue'
                 </div>
             </div>
             <div class="row">
-               <div class="col">
-                <b>Philippines</b>
-               </div>
+                <div class="col">
+                    <b>Philippines</b>
+                </div>
             </div>
             <div class="row">
                 <div class="col">
-                    <input type="text" placeholder="Mobile No." >
+                    <input type="text" placeholder="Mobile No.">
                 </div>
                 <div class="col">
                     <input type="text" placeholder="Floor Unit No.">
@@ -49,23 +100,23 @@ import Footer from '@/components/Client_Footer.vue'
             </div>
             <div class="row">
                 <div class="col">
-                   <select name="" id="" class="form-select">
-                    <option value=""></option>
-                   </select>
+                    <select name="" id="" class="form-select">
+                        <option value=""></option>
+                    </select>
                 </div>
             </div>
             <div class="row">
                 <div class="col">
                     <select name="" id="" class="form-select">
                         <option value=""></option>
-                       </select>
+                    </select>
                 </div>
             </div>
             <div class="row">
                 <div class="col">
                     <select name="" id="" class="form-select">
                         <option value=""></option>
-                       </select>
+                    </select>
                 </div>
             </div>
             <div class="row">
@@ -84,13 +135,15 @@ import Footer from '@/components/Client_Footer.vue'
                         <thead>
                             <tr>
                                 <th>PRODUCT</th>
+                                <th>Qty</th>
                                 <th>TOTAL</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>VLADDIN Vantage Pod Cartridge 1.6ml MTL 1.2 Ohms  × 1</td>
-                                <td><b>P800.00</b></td>
+                            <tr v-for="(data, index) in order" :key="index">
+                                <td>{{ data.product_label }}</td>
+                                <td> <small>x{{ data.quantity }}</small></td>
+                                <td><b>P{{ data.price * data.quantity }}</b></td>
                             </tr>
                         </tbody>
                     </table>
@@ -112,58 +165,63 @@ import Footer from '@/components/Client_Footer.vue'
                     <span>Total</span>
                 </div>
                 <div class="col text-end">
-                    <b>P800</b>
+                    <b>P{{ overAllTotalParams }}</b>
                 </div>
                 <hr>
             </div>
             <div class="row">
                 <div class="col text-end">
-                    <button class="btn btn-dark">PLACE ORDER</button>
+                    <button class="btn btn-dark" @click="submit">PLACE ORDER</button>
                 </div>
             </div>
 
         </div>
-      
-     
-        
+
+
+
     </section>
-    <Footer/>
-   
+    <Footer />
+
 </template>
 <style scoped>
 section {
     width: 70%;
     margin: auto;
 }
+
 .section-one {
     display: flex;
-    gap:10px;
+    gap: 10px;
     justify-content: center;
 }
+
 .address {
     display: grid;
-    gap:10px;
+    gap: 10px;
     box-shadow: 0px 0px 2px gray;
-    padding:10px;
+    padding: 10px;
     margin-top: 20px;
     border-radius: 3px;
     background-color: rgb(248, 246, 237);
 
 }
-.address input{
-    padding:15px;
+
+.address input {
+    padding: 15px;
     border-radius: 5px;
-    border:solid 1px gray;
+    border: solid 1px gray;
 }
+
 .address select {
-    padding:15px;
-    border:solid 1px gray;
+    padding: 15px;
+    border: solid 1px gray;
 }
-.your-order{
+
+.your-order {
     display: grid;
-    gap:10px;
+    gap: 10px;
     border: solid 1px black;
-    padding:10px;
+    padding: 10px;
     margin-top: 20px;
     border-radius: 3px;
     background-color: rgb(255, 255, 255);
