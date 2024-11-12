c017e6c9fa006071e39bb432533b902ff6653982
Kiel Alarcon Bermudez <kielbermudez200104@gmail.com>
2024-11-12 13:49:06 +0800

    admin orders UI fixed

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,3 +1,7 @@
+<script setup>
+import ChatBox from '@/components/Client_Chat_Box.vue'
+</script>
 <template>
     <router-view></router-view>
+   <ChatBox/>
  </template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,34 @@
 import "./bootstrap";
-
+import 'primeicons/primeicons.css'
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { createPinia } from "pinia";
-import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import ConfirmationService from "primevue/confirmationservice";
+import DialogService from "primevue/dialogservice";
+import ToastService from "primevue/toastservice";
 
 
 const app = createApp(App);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            prefix: 'p',
+            darkModeSelector: 'light',
+            cssLayer: false
+        }
+    }
+ });
 const pinia = createPinia();
+app.use(ConfirmationService);
+app.use(ToastService);
+app.use(DialogService);
 app.use(router);
 app.use(pinia);
-app.use(ElementPlus);
+app.use(PrimeVue);
 app.mount("#app");
