d8e9c7d53dac18b21b3922fec85a14cadf777d4b
Kiel Alarcon Bermudez <kielbermudez200104@gmail.com>
2024-11-14 22:35:49 +0800

    Footer added on cancellation

--- resources/js/pages/client/Client_Cancelation.vue
+++ resources/js/pages/client/Client_Cancelation.vue
@@ -1,5 +1,5 @@
 <script setup>
-import Header from '@/components/Client_Header.vue'
+import Footer from '@/components/Client_Footer.vue'
 import Navbar from '@/components/Client_Navbar.vue'
 import NavbarCategory from '@/components/Client_Navbar_Category.vue'
 import NavbarAccount from '@/components/Client_Navbar_Account.vue'
@@ -91,6 +91,7 @@ onMounted (() => {
         </tbody>
        </table>
     </section>
+    <Footer/>
 </template>
 
 <style scoped>
