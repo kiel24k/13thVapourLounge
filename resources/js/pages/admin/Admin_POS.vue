10e07156e99e000c517e198c25bc0a0f7b9dfbd1
Kiel Alarcon Bermudez <kielbermudez200104@gmail.com>
2024-11-26 23:51:41 +0800

    tesa5

--- resources/js/pages/admin/Admin_POS.vue
+++ resources/js/pages/admin/Admin_POS.vue
@@ -57,7 +57,7 @@ watch(selectedCustomer, (oldVal, newVal) => {
 
 watch(selectedItemList, (oldVal, newVal) => {
     POS_ITEM_API()
-    "dsddadasdasdsdsa"
+    "sdsa"
 })
 
 watch(search, (oldVal, newVal) => {
