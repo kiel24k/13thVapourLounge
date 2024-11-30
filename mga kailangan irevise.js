3c9743967e5a2e620fdbda3b7b63b3ff567c186b
Kiel Alarcon Bermudez <kielbermudez200104@gmail.com>
2024-12-01 01:52:42 +0800

    delete: 'DONE'

---
+++ mga kailangan irevise.js
@@ -0,0 +1,13 @@
+const Admin = [
+    POS [
+        {calculate_every_price: 'DONE'},
+        {delete: 'DONE'},
+        {calculate_quantity: '' },
+        {calculate_grand_total: ''},
+        {reserve_modal: ''},
+        {save_reserve_on_database: ''},
+        {create_pdf_receipt_after_press_the_cash_button:''}
+    ],
+
+    
+]
