20ca2888c4b024377a383ccef4c446a54ca6cff2
Kiel Alarcon Bermudez <kielbermudez200104@gmail.com>
2024-12-05 23:12:09 +0800

    USERS task added

--- mga kailangan irevise.js
+++ mga kailangan irevise.js
@@ -1,19 +1,23 @@
 const Admin = [
     ORDERS[
-        {SEARCH_AND_CATEGORY: ''},
-        {SELECTED_NAME_NOT_DISPLAYING: ''},
+        { SEARCH_AND_CATEGORY_AND_PAGINATION: "" },
+        { SELECTED_NAME_NOT_DISPLAYING: "" },
+        {}
+    ],
+    PRODUCTS[{ SEARCH_AND_CATEGORY_AND_PAGINATION_AND_SORTING: "" }
         
     ],
-    POS [
-        {calculate_every_price: 'DONE'},
-        {delete: 'DONE'},
-        {calculate_quantity: 'DONE' },
-        {calculate_grand_total: 'DONE'},
-        {reserve_modal: 'DONE'},
-        {save_reserve_on_database: 'DONE'},
-        {create_modal_receipt: 'DONE'},
-        {create_pdf_receipt_after_press_the_cash_button:''}
+    USERS[
+        {}
     ],
-
-                    
-]
+    POS[
+        { calculate_every_price: "DONE" },
+        { delete: "DONE" },
+        { calculate_quantity: "DONE" },
+        { calculate_grand_total: "DONE" },
+        { reserve_modal: "DONE" },
+        { save_reserve_on_database: "DONE" },
+        { create_modal_receipt: "DONE" },
+        { create_pdf_receipt_after_press_the_cash_button: "" }
+    ],
+];
